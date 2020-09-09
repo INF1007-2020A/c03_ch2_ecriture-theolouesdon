fd932df6e8673558cd6ace03a1124f0ab171b966
theolouesdon <65035689+theolouesdon@users.noreply.github.com>
2020-09-09 11:56:42 -0300

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,8 +4,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        maj = ord(lettre)-32
-        lettre = chr(maj)
+        lettre = chr(ord(lettre)-32)
         resultat += lettre
     return resultat
 
